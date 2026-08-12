3cba8243370ebf224ce2dc4c34787fe80662ab2a
skam <skam@cs144.edu>
2026-08-12 20:12:41 +0800

    Lab 2: complete TCPReceiver implementation

--- libsponge/tcp_receiver.cc
+++ libsponge/tcp_receiver.cc
@@ -11,9 +11,27 @@ void DUMMY_CODE(Targs &&... /* unused */) {}
 using namespace std;
 
 void TCPReceiver::segment_received(const TCPSegment &seg) {
-    DUMMY_CODE(seg);
+    const TCPHeader &header = seg.header();
+    //! first segment received, set isn
+    if(header.syn) {
+        _isn = header.seqno;
+        _syn_received = true;
+        if(seg.payload().str().empty() && !header.fin) return ;
+    }
+    //! if syn not set, ignore other segment
+    if(!_syn_received) return ;
+    uint64_t checkpoint = _reassembler.stream_out().bytes_written() + 1;
+    uint64_t abs_seqno = unwrap(header.seqno, _isn.value(), checkpoint);
+    uint64_t stream_index = header.syn ? abs_seqno : abs_seqno - 1; //! consider overflow when abs_seqno equals to 0
+    if(header.fin) _fin_received = true;
+    _reassembler.push_substring(seg.payload().copy(), stream_index, header.fin);
 }
 
-optional<WrappingInt32> TCPReceiver::ackno() const { return {}; }
+optional<WrappingInt32> TCPReceiver::ackno() const {  
+    if(!_syn_received) return nullopt;
+    uint64_t abs_ackno = _reassembler.stream_out().bytes_written() + 1;
+    if(stream_out().input_ended()) abs_ackno += 1;
+    return wrap(abs_ackno, _isn.value());
+}
 
-size_t TCPReceiver::window_size() const { return {}; }
+size_t TCPReceiver::window_size() const { return _capacity - _reassembler.stream_out().buffer_size(); }
--- libsponge/wrapping_integers.cc
+++ libsponge/wrapping_integers.cc
@@ -14,8 +14,10 @@ using namespace std;
 //! \param n The input absolute 64-bit sequence number
 //! \param isn The initial sequence number
 WrappingInt32 wrap(uint64_t n, WrappingInt32 isn) {
-    DUMMY_CODE(n, isn);
-    return WrappingInt32{0};
+    uint32_t isn_raw = isn.raw_value();
+    uint32_t n_truncated = static_cast<uint32_t>(n);
+    uint32_t result = isn_raw + n_truncated;
+    return WrappingInt32(result);
 }
 
 //! Transform a WrappingInt32 into an "absolute" 64-bit sequence number (zero-indexed)
@@ -29,6 +31,12 @@ WrappingInt32 wrap(uint64_t n, WrappingInt32 isn) {
 //! and the other stream runs from the remote TCPSender to the local TCPReceiver and
 //! has a different ISN.
 uint64_t unwrap(WrappingInt32 n, WrappingInt32 isn, uint64_t checkpoint) {
-    DUMMY_CODE(n, isn, checkpoint);
-    return {};
+    uint32_t offset = n.raw_value() - isn.raw_value();
+    uint64_t candidate = (checkpoint & 0xFFFFFFFF00000000ULL) + offset;
+    if(candidate > checkpoint + (1ULL << 31)&& candidate >= (1ULL << 32)) {
+        candidate -= (1ULL << 32);
+    } else if(candidate + (1ULL << 31) < checkpoint) {
+        candidate += (1ULL << 32); 
+    }
+    return candidate;
 }
